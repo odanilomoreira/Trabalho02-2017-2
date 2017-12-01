02b50811627090091c410ba94a20984df9ec5c6d
odanilomoreira <danilokof7@gmail.com>
2017-12-01 15:26:24 -0200

    set up ListView customizado

--- app/src/main/java/danilo/com/trabalho02_2017_2/MainActivity.java
+++ app/src/main/java/danilo/com/trabalho02_2017_2/MainActivity.java
@@ -12,11 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView list_eventos = findViewById(R.id.list_eventos);
-
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.atributos, android.R.layout.simple_list_item_1);
-        list_eventos.setAdapter(adapter);
-
 
 
 
