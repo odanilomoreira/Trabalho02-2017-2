f91041ca7fb8c28c397152cdd52cd47596554957
odanilomoreira <danilokof7@gmail.com>
2017-12-01 15:05:25 -0200

    exemplo basico de listView

--- app/src/main/java/danilo/com/trabalho02_2017_2/MainActivity.java
+++ app/src/main/java/danilo/com/trabalho02_2017_2/MainActivity.java
@@ -2,6 +2,8 @@ package danilo.com.trabalho02_2017_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView list_eventos = findViewById(R.id.list_eventos);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.atributos, android.R.layout.simple_list_item_1);
+        list_eventos.setAdapter(adapter);
+
+
+
+
+
+
+
+
+
     }
 }
